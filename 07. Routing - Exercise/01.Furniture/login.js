6247b6f5d255c3ba736b233aca8c2658192941c4
Dimcho Lakov <mdsektor@gmail.com>
2021-03-24 00:30:16 +0000

    Add validation for login

--- 07. Routing - Exercise/01.Furniture/login.js
+++ 07. Routing - Exercise/01.Furniture/login.js
@@ -1,7 +1,8 @@
 import { html } from './node_modules/lit-html/lit-html.js';
+import { classMap } from './node_modules/lit-html/directives/class-map.js';
 import * as api from './data.js';
 
-const loginTemplate = (onSubmit) => html`
+const loginTemplate = (onSubmit, errorMessage, isEmailInvalid, isPasswordInvalid) => html`
     <div class="row space-top">
         <div class="col-md-12">
             <h1>Login User</h1>
@@ -11,13 +12,18 @@ const loginTemplate = (onSubmit) => html`
     <form @submit=${onSubmit}>
         <div class="row space-top">
             <div class="col-md-4">
+                ${errorMessage ? html`
+                <div class="form-group">
+                    <p class="text-danger">${errorMessage}</p>
+                </div>` : ''}
                 <div class="form-group">
                     <label class="form-control-label" for="email">Email</label>
-                    <input class="form-control" id="email" type="text" name="email">
+                    <input class=${classMap(getValidationClasses(isEmailInvalid))} id="email" type="text" name="email">
                 </div>
                 <div class="form-group">
                     <label class="form-control-label" for="password">Password</label>
-                    <input class="form-control" id="password" type="password" name="password">
+                    <input class=${classMap(getValidationClasses(isPasswordInvalid))} id="password" type="password"
+                        name="password">
                 </div>
                 <input type="submit" class="btn btn-primary" value="Login" />
             </div>
@@ -31,12 +37,21 @@ export function loginPage(ctx) {
     async function onSubmit(event) {
         event.preventDefault();
         const formData = new FormData(event.target);
-    
+
         const email = formData.get('email');
         const password = formData.get('password');
-    
+
+        if (email === '' || password === '') {
+            ctx.render(loginTemplate(onSubmit, 'All fields are required!', email === '', password === ''));
+            return false;
+        }
+
         await api.login(email, password);
-    
+
         ctx.page.redirect('/');
     }
+}
+
+function getValidationClasses(isInputInvalid) {
+    return isInputInvalid ? { 'form-control': true, 'is-invalid': true } : { 'form-control': true };
 }
